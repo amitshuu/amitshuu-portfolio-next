ed75c1f53c4c091f450b91dea160870b80cbd3b5
amitshuu <amitshuu@gmail.com>
2023-04-09 13:15:53 +0300

    Seo changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,10 @@ export const metadata = {
       "I am a fullstack web developer offering web development services including creating landing pages and complex web applications, Shopify ecommerce development, website design, and website maintenance. Contact me to bring your website to life!",
     type: "website",
     images: {
-      url: "https://amitshuu-portfolio-next.vercel.app/computer.png",
+      url: "https://amitshuu-portfolio-next.vercel.app/shuki_hero_image.png",
       width: 800,
       height: 600,
-      alt: "A description of the image",
+      alt: "Amit Shukrun - Web Developer",
     },
   },
   keywords:
