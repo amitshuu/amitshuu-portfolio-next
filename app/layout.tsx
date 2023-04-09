0181fd7efb58fd3346f9846fe5b161f4c7e0a1bd
amitshuu <amitshuu@gmail.com>
2023-04-09 13:22:28 +0300

    SEO image height\width

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata = {
     type: "website",
     images: {
       url: "https://amitshuu-portfolio-next.vercel.app/shuki_hero_image.png",
-      width: 800,
-      height: 600,
+      width: 1200,
+      height: 630,
       alt: "Amit Shukrun - Web Developer",
     },
   },
