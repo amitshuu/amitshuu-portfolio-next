ba5b7ffd6b9b60a9530953c8c08451befc87e717
amitshuu <amitshuu@gmail.com>
2023-04-20 12:16:28 +0300

    Seo fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,8 @@ export const metadata = {
       "I'm Amit Shukrun, a fullstack web developer offering landing page creation, web app development, Shopify ecommerce and more. Let's build your website!",
     type: "website",
     images: [
-      // {
-      //   url: "http://www.amitshuu.com/laptop.png",
-      //   width: 800,
-      //   height: 600,
-      //   alt: "Amit Shukrun amitshuu עמית שוקרון",
-      // },
       {
-        url: "http://www.amitshuu.com/laptop.png",
+        url: "https://www.amitshuu.com/laptop.png",
         width: 1200,
         height: 630,
         alt: "Amit Shukrun amitshuu עמית שוקרון",
@@ -28,8 +22,9 @@ export const metadata = {
   other: {
     "google-site-verification": "7xdzd4T16clU3DEhxgD69Da7KLy1-uWqj9s8xHWxZWU",
   },
-  keywords:
-    "Amit Shukrun, amitshuu, Web development, Full-stack development, Landing page design, Website design, Shopify development, Website maintenance, Custom web applications, Web design services, עמית שוקרון, שוקרון",
+  keywords: [
+    "Amit Shukrun, amitshuu, Shukrun, מפתח אתרים, דפי נחיתה, Web development, Full-stack development, Landing page design, Website design, Shopify development, Website maintenance, Custom web applications, Web design services, עמית שוקרון, שוקרון",
+  ],
   title: "Amit Shukrun - Web Developer | עמית שוקרון - מפתח אתרים",
   description:
     "I'm Amit Shukrun, a fullstack web developer offering landing page creation, web app development, Shopify ecommerce and more. Let's build your website!",
